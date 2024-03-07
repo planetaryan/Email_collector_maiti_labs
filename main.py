3b961f15d91d1016fbfebe9167fe17bd4473ea64
planetaryan <117113732+planetaryan@users.noreply.github.com>
2024-03-07 17:36:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,6 @@ from urllib.parse import urlsplit, urljoin, unquote
 from tld import get_fld  
 from tld.exceptions import TldDomainNotFound 
 
-import flet as ft
-import os
-
-
-# set Flet path to an empty string to serve at the root URL (e.g., https://lizards.ai/)
-# or a folder/path to serve beneath the root (e.g., https://lizards.ai/ui/path
-DEFAULT_FLET_PATH = ''  # or 'ui/path'
-DEFAULT_FLET_PORT = 8502
 
 def main(page: ft.Page):
 
@@ -120,6 +112,5 @@ def main(page: ft.Page):
     
 
 if __name__ == "__main__":
-    flet_path = os.getenv("FLET_PATH", DEFAULT_FLET_PATH)
-    flet_port = int(os.getenv("FLET_PORT", DEFAULT_FLET_PORT))
+
     ft.app(name=flet_path, target=main, view=None, port=flet_port)
